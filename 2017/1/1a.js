5d98d104e0dbdfae0240f702c7c33e1edbdd011b
Matt Tingen <matt.tingen@gmail.com>
2017-12-02 14:24:03 -0500

    Automate challenge running

--- 2017/1/1a.js
+++ 2017/1/1a.js
@@ -2,7 +2,4 @@ const main = require('../../util/main');
 const sumDigits = require('./sumDigits');
 
 module.exports = exports = sumDigits((i, length) => (i + 1) % length);
-
-if (!module.parent) {
-  main(exports);
-}
+main(exports);
--- 2017/1/1b.js
+++ 2017/1/1b.js
@@ -2,7 +2,4 @@ const main = require('../../util/main');
 const sumDigits = require('./sumDigits');
 
 module.exports = exports = sumDigits((i, length) => (i + length / 2) % length);
-
-if (!module.parent) {
-  main(exports);
-}
+main(exports);
